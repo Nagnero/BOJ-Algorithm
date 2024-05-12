ac9e08cfe9f46d67d6a574ad7827651b3d1f641d
sunwoo <davidyeon123@naver.com>
2024-05-12 23:01:01 +0900

    1110, 1934

---
+++ math/BOJ_1934_LCM2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.close();
+
+        int cnt = 0;
+        int temp = N;
+        while (true) {
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+            cnt++;
+
+            if (temp == N) {
+                break;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
+ 
